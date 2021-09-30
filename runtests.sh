a9e3ae1e98d0359ce8a299ae3724505431bdad7e
Markus Jung <markus.jung@kit.edu>
2021-09-30 14:24:08 +0200

    update to current master

--- runtests.sh
+++ runtests.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-# USAGE: do NOT execute this script, "source" it from your shell
-#	. runtests.sh
+
+inPar=true
+
+while getopts pnh name
+do
+    case $name in
+    p)   inPar=true;;
+    n)   inPar=false;;    
+    h)	 echo "Script to run all the Matlab test cases.";
+ 	 echo "Available options:"
+	 echo "p:      Runs the test cases in parallel, with the number of jobs dependent on the Matlab installation and the number of physical cores. This is the default option and used if called without argument";
+	 echo "n:      Runs the test cases sequentially, one after the other.";	
+	 echo "h:      Print this help.";
+	exit 0;;
+    ?)   echo "Usage: runtests [-p] [-n] [-h]";
+        exit 2;;
+    esac
+done
 
 cd libncs_matlab
-make clean
-make -j 4
+make cleanmex
+make mex -j 4
 cd ..
-matlab -nodisplay -r "addpath(genpath('libncs_matlab/tests'));addpath(genpath('matlab/Tests')); addpath(genpath('matlab/Classes'));addpath(genpath('matlab/external'));addpath(genpath('matlab/functions')); addpath(genpath('matlab/out')); results=[executeTestsMatlab() executeTestsLibNcsMatlab()];exit(any([results.Failed]));"
+matlab -nodisplay -r "addpath(genpath('libncs_matlab/tests'));addpath(genpath('matlab/Tests')); addpath(genpath('matlab/Classes'));addpath(genpath('matlab/external'));addpath(genpath('matlab/functions')); addpath(genpath('matlab/out')); results=[executeTestsMatlab($inPar) executeTestsLibNcsMatlab($inPar)];exit(any([results.Failed]));"
 
